60f85ef8fffe7a0de42cc394024662134c54ee3a
Laura Elder <elderrrlr@gmail.com>
2021-12-03 20:25:03 -0800

    added in the other pages for patterns and updated to elaines new service code

--- SoftwareOneWebPage-LauraElderStocks/app.py
+++ SoftwareOneWebPage-LauraElderStocks/app.py
@@ -46,33 +46,6 @@ def patternsPage():
     # Render the page
     return render_template('patterns.html')
 
-@app.route('/Hammer')
-def pattern_returnPage():
-    pattern_pass = "Hammer"
-    signal = "Buy"
-    image = "someone"
-    #call Elain's service
-    user_query = 'candlestick pattern'
-    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
-    infor=display_info.display_info(user_query, location)
-    #go get the data from service
-    # Opening JSON file
-    f = open(location,)
-    # returns JSON object as
-    # a dictionary
-    data = json.load(f)
-    # Iterating through the json
-    max = data['results']
-    oi = dict([(ma['summary'], ma) for ma in max])
-    going =""
-    for j in oi:
-        if going == "":
-            going = going + j
-    information = going
-    # Closing file
-    f.close()
-    return render_template('pn.html', patternZ = pattern_pass, signal=signal, image=image, information=information)
-
 @app.route('/entry', methods = ['POST', 'GET'])
 def entry():
     if request.method == 'POST':
@@ -90,9 +63,9 @@ def searchPage(name):
    filename ='C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\stocks.json'
    with open(filename,'w') as file_object:
     json.dump(data,file_object)
-    price = '100.00'
-    action = "Buy"
-    pattern="Hammer"
+    price = '100.00'#change this Laura
+    action = "Buy"#change this Laura
+    pattern="Hammer"#change this Laura
    return render_template('analysis.html', name =name, price = price, action= action, pattern=pattern)
    #return data
    #return quote.info
@@ -102,6 +75,133 @@ def display_quote():
   symbol = request.args.get('symbol', default="AAPL")
   quote = yf.Ticker(symbol)
   return quote.info
+
+@app.route('/Hammer')
+def pattern_returnPage():
+    pattern_pass = "Hammer"
+    signal = "Buy"
+    #image = "https://en.wikipedia.org/wiki/Hammer_(candlestick_pattern)#/media/File:Hammer_pattern_03.jpg"
+    #call Elain's service
+    user_query = 'Hammer_(candlestick_pattern)'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, information=information)
+
+@app.route('/Inverse')
+def pattern_returnPageI():
+    pattern_pass = "Inverse Hammer/ Inverted Hammer"
+    signal = "Buy"
+    image = "https://hitandruncandlesticks.com/wp-content/uploads/2014/11/Inverted-Hammer-Sample-1.png"
+    #call Elain's service
+    user_query = 'Inverted_hammer'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, information=information, image=image)
+
+@app.route('/Morning')
+def pattern_returnPageM():
+    pattern_pass = "Morning Star"
+    signal = "Buy"
+    image="https://cdn.howtotradeblog.com/wp-content/uploads/2020/06/01224309/structure-of-a-morning-star-pattern.jpg"
+    #call Elain's service
+    user_query = 'Morning_star_(candlestick_pattern)'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, information=information, image=image)
+
+@app.route('/White')
+def pattern_returnPageW():
+    pattern_pass = "Three White Soldiers"
+    signal = "Buy"
+    image=str("https://a.c-dn.net/c/content/dam/publicsites/igcom/uk/images/content-2-chart-images/three-white-soldiers.png")
+    #call Elain's service
+    user_query = 'Three_white_soldiers'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, information=information, image=image)
+
+@app.route('/Hanging')
+def pattern_returnPageH():
+    pattern_pass = "Hanging Man"
+    signal = "Sell"
+    image="https://www.forexboat.com/wp-content/uploads/2020/02/Hanging-Man-Candlestick-Pattern-Summary.jpg"
+    #call Elain's service
+    user_query = 'Hanging_man_(candlestick_pattern)'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, information=information, image=image)
+
+@app.route('/Crow')
+def pattern_returnPageCRO():
+    pattern_pass = "Three Black Crows"
+    signal = "Sell"
+    image="https://www.brameshtechanalysis.com/wp-content/uploads/2010/12/three_black_crows.png"
+    #call Elain's service
+    user_query = 'Three_black_crows'
+    location = 'C:\\Users\\Laura\\source\\repos\\SoftwareOneWebPage-LauraElderStocks\\search_results.json'
+    infor=display_info.display_info(user_query, location)
+    #go get the data from service
+    # Opening JSON file
+    f = open(location,)
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+    # Iterating through the json
+    information = data['summary'] #this has changed
+    # Closing file
+    f.close()
+    return render_template('pn.html', patternZ = pattern_pass, signal=signal, image=image, information=information)
+
 
 if __name__ == '__main__':
     # Run the app server on localhost:4449
--- SoftwareOneWebPage-LauraElderStocks/display_info.py
+++ SoftwareOneWebPage-LauraElderStocks/display_info.py
@@ -1,25 +1,13 @@
-# CS361 Assignment 5: MVP Microservices
+# CS361 Assignment 7: Microservices Integration
 # Author: Elaine Zamora
-# Date: 10/27/2021
+# Date: 11/11/2021
 # 
 
 import sys
-import argparse
 import json
 import requests
 import wikipedia
-import demjson
 
-url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="
-
-# ------------------------
-def join_words(words):
-    """ This takes list of words entered by the user and reformats it 
-        separated by "_" where wordX is a word entered and X is the Xith
-        word in the list: word1_word2_word3
-    """
-    search_q = "_".join(words)
-    return search_q
 
 
 def display_info(words, location):
@@ -27,28 +15,33 @@ def display_info(words, location):
         Takes as input a query and writes result to a text file named search_result.txt
         Uses Wikipedia to search and return a summary of text related to search query
         """
-    # join query words separated by underscore to append to url
-    query_name = join_words(words)
+    to_file = location
 
-    # search wikipedia, parse data, and write summary to search_result.txt
-    summary = wikipedia.summary(query_name)
-    #print(summary)
-    data = {"name":query_name, "summary":summary}
-    filename = location
-    var = [data]
-    #newVar=demjson.encode(var)
-    lastvar = {"results":var}
-    with open(filename,'w') as file_object:
-        json.dump(lastvar,file_object)
+   # search wikipedia, parse data, and write summary to search_result.txt
+    try: 
+        summary = wikipedia.summary(words)
+    except:
+        temp = {'name':words, 'summary':'NULL'}
+        with open(to_file, "w") as outfile:
+            json.dump(temp, outfile)
+        outfile.close()
+        
+
+    #create dictionary to store results for user to access
+    r_dict = {'name':words, 'summary':summary}
+
+    # write results of name, summary, image to json outfile
+    with open(to_file, "w") as outfile:
+        json.dump(r_dict, outfile)
+    outfile.close()
 
 
-if __name__ == "__main__":
-    # Note: The following can be utilized via CLI with search terms 
-    # immediately after the filename separated by spaces
-    arg = sys.argv
-    display_info(arg[1:], location)
-    
-    # Note: You can use the following to simply call the function with search terms as args
-    #     : user query must be entered with spaces between terms
-    # user_query = 'Nelson Mandela'
-    # display_info(user_query)
+
+def main():
+    # example usage
+    #user_query = 'oprah'
+    #file_location = 'C:\\Users\\elain\\361\\search_result.txt'
+    #display_info(user_query, file_location)
+
+    if __name__ == "__main__":
+        main()
